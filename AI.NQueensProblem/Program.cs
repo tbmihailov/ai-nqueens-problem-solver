077c576f7940aa5190f524962fe369f225d1a8a4
Todor B. Mihaylov <tbmihailov@gmail.com>
2014-01-02 00:50:07 +0200

    Initial Solution and project

---
+++ AI.NQueensProblem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI.NQueensProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 8;
+            Solver solver = new Solver(n);
+            solver.Solve();
+        }
+    }
+}
