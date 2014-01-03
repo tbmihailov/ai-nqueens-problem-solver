4ba3b60df2ccae9aa7118aaf561b5213509949d3
Todor B. Mihaylov <tbmihailov@gmail.com>
2014-01-03 02:21:05 +0200

    Algorithm is wrong

--- AI.NQueensProblem/Solver.cs
+++ AI.NQueensProblem/Solver.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,40 +21,70 @@ namespace AI.NQueensProblem
         {
             get { return _stepsLog; }
         }
-        
-        private void Solve(int boardSize)
+
+        public void Solve(int boardSize)
         {
             Board board = new Board(boardSize);
             board.InitFigurePositions();
+            Console.WriteLine("{0} queens", boardSize);
+            Console.WriteLine("Initial board:");
+            Console.WriteLine(board.ToString());
 
             int stepCount = 0;
-            while (board.Collisions() > 0)
+            int queenIndex = 0;
+            while (board.CollisionsCnt > 0)
             {
-                Figure queen = board.GetQueenWithMaxCollisions();
+                Figure queen = board.Collisions.Select(kv=>kv.Key).ToArray()[queenIndex];
+
+                int previousRow = queen.Row;
+                int previousColumn = queen.Column;
+
+                int queenRow = queen.Row;
+                int queenColumn = queen.Column;
 
                 //move queen in column;
-                int newColumn = 0;
+                int actualCollisions = board.CollisionsCnt;
+                int bestCollisions = actualCollisions;
 
-                int actualCollisions = board.Collisions();
-                while (board.TestCollisionsForMove(queen, queen.Row, newColumn) >= actualCollisions && newColumn<boardSize)
+                int newColumn = 0;
+                while (newColumn<boardSize)
                 {
+                    board.TryMove(queen, queenRow, newColumn);
+                    int currentCollisions = board.CollisionsCnt;
+                    if (currentCollisions <= bestCollisions)
+                    {
+                        bestCollisions = currentCollisions;
+                        queenColumn = newColumn;
+                    }
                     newColumn++;
                 }
 
-                if (newColumn != boardSize)//max column - no better position for this queen
+                board.TryMove(queen, queenRow, queenColumn);
+
+                if (previousColumn == queenColumn && previousRow == queenRow)
+                {
+                    queenIndex++;
+                }
+                else
                 {
                     stepCount++;
-
-                    AddStepLog(stepCount,queen.Id, queen.Row, queen.Column, queen.Row, newColumn);
-                    board.Move(queen, queen.Row, newColumn);
+                    AddStepLog(stepCount, queen.Id, queen.Row, queen.Column, queen.Row, newColumn, board);
+                    queenIndex = 0;
                 }
             }
+
+            Console.WriteLine("=========================");
+            Console.WriteLine("Solution in {0} steps", stepCount);
         }
 
-        private void AddStepLog(int stepNumber,int queenId, int fromRow, int fromColumn, int toRow, int toColumn)
+        private void AddStepLog(int stepNumber,int queenId, int fromRow, int fromColumn, int toRow, int toColumn, Board board)
         {
             var figureMove = new FigureMove(stepNumber,queenId, fromRow, fromColumn, toRow, toColumn);
             StepsLog.Add(figureMove);
+
+            Console.WriteLine("Step:{0}", figureMove.StepNumber);
+            Console.WriteLine(board);
+            Console.WriteLine("Collisions {0}", board.CollisionsCnt);
         }
 
         internal void Solve()
