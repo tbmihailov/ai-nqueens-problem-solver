d15046b8e32a8871bc534ffb9fec9b88600d3932
Todor B. Mihaylov <tbmihailov@gmail.com>
2014-01-03 02:20:44 +0200

    Implemented collision detection and move

--- AI.NQueensProblem/Board.cs
+++ AI.NQueensProblem/Board.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace AI.NQueensProblem
 {
-    class Board
+    public class Board
     {
         private int _boardSize;
 
@@ -33,43 +33,171 @@ namespace AI.NQueensProblem
             {
                 var random = new Random();
                 int availableRow = random.Next(figuresToPlace);
-                int availableColumn = random.Next(10, figuresToPlace+10)-10;
+                int randomStep = availableRow * availableRow + _boardSize;
+                int availableColumn = random.Next(randomStep, figuresToPlace + randomStep) - randomStep;
 
                 int newRow = availableRows[availableRow];
                 int newColumn = availableColumns[availableColumn];
 
-                var figure = new Figure(newRow,newColumn,_boardSize,_boardSize - figuresToPlace + 1);
+                var figure = new Figure(newRow, newColumn, _boardSize, _boardSize - figuresToPlace + 1);
                 _figures.Add(figure);
                 availableRows.RemoveAt(availableRow);
                 availableColumns.RemoveAt(availableColumn);
                 figuresToPlace--;
             }
-        }
 
+            CalculateCollisions();
+        }
 
         public bool AreInCollision(Figure figure1, Figure figure2)
         {
             throw new NotImplementedException();
         }
 
-        internal int Collisions()
+
+        public int CollisionsCnt
         {
-            throw new NotImplementedException();
+            get
+            {
+                return _collisions.Count;
+            }
         }
 
-        internal Figure GetQueenWithMaxCollisions()
+        Dictionary<Figure, int> _collisions = new Dictionary<Figure, int>();
+        public Dictionary<Figure, int> Collisions
         {
-            throw new NotImplementedException();
+            get { return _collisions; }
         }
+
+        public void CalculateCollisions()
+        {
+            _collisions.Clear();
+            var figures = _figures;
+
+            int collisionsCnt = 0;
+
+            //row collisions
+            var rowCollisions = from f in figures
+                                group f by f.Row into g
+                                where g.Count() > 1
+                                select new { Row = g.Key, Figures = g.OrderBy(f => f.Column) };
+            //collisionsCnt += rowCollisions.Sum(g => g.Figures.Count() - 1);
+            foreach (var rowCollision in rowCollisions)
+            {
+                foreach (var figure in rowCollision.Figures)
+                {
+                    if (!_collisions.ContainsKey(figure))
+                    {
+                        _collisions.Add(figure, 0);
+                    }
+                    _collisions[figure] += 1;
+                    collisionsCnt++;
+                }
+                collisionsCnt--;//collisions are figures-1
+            }
+
+            //column collisions
+            var columnCollisions = from f in figures
+                                   group f by f.Column into g
+                                   where g.Count() > 1
+                                   select new { Column = g.Key, Figures = g.OrderBy(f => f.Row) };
+            //collisionsCnt += columnCollisions.Sum(g => g.Figures.Count() - 1);
+            foreach (var colCollision in columnCollisions)
+            {
+                foreach (var figure in colCollision.Figures)
+                {
+                    if (!_collisions.ContainsKey(figure))
+                    {
+                        _collisions.Add(figure, 0);
+                    }
+                    _collisions[figure] += 1;
+                    collisionsCnt++;
+                }
+                collisionsCnt--;//collisions are figures-1
+            }
+
+            //diagonal1 collisions
+            var d1Collisions = from f in figures
+                               group f by f.Diagonal1 into g
+                               where g.Count() > 1
+                               select new { Row = g.Key, Figures = g.OrderBy(f => f.Column) };
+            //collisionsCnt += d1Collisions.Sum(g => g.Figures.Count() - 1);
+            foreach (var d1Collision in d1Collisions)
+            {
+                foreach (var figure in d1Collision.Figures)
+                {
+                    if (!_collisions.ContainsKey(figure))
+                    {
+                        _collisions.Add(figure, 0);
+                    }
+                    _collisions[figure] += 1;
+                    collisionsCnt++;
+                }
+                collisionsCnt--;//collisions are figures-1
+            }
+
+            //column collisions
+            var d2Collisions = from f in figures
+                               group f by f.Diagonal2 into g
+                               where g.Count() > 1
+                               select new { Column = g.Key, Figures = g.OrderBy(f => f.Column) };
+            //collisionsCnt += d2Collisions.Sum(g => g.Figures.Count() - 1);
+            foreach (var d2Collision in d2Collisions)
+            {
+                foreach (var figure in d2Collision.Figures)
+                {
+                    if (!_collisions.ContainsKey(figure))
+                    {
+                        _collisions.Add(figure, 0);
+                    }
+                    _collisions[figure] += 1;
+                    collisionsCnt++;
+                }
+                collisionsCnt--;//collisions are figures-1
+            }
+
+            _collisions = _collisions.OrderByDescending(c => c.Value).ToDictionary(k => k.Key, v => v.Value);
+        }
+
+
 
         internal int TestCollisionsForMove(Figure queen, int p, int newColumn)
         {
             throw new NotImplementedException();
         }
 
-        internal void Move(Figure queen, int p, int newColumn)
+        internal bool TryMove(Figure queen, int newRow, int newColumn)
         {
-            throw new NotImplementedException();
+            if (_figures.Any(f => f.Row == newRow && f.Column == newColumn))
+            {
+                return false;
+            }
+            else
+            {
+                queen.SetPosition(newRow, newColumn);
+                CalculateCollisions();
+                return true;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder boardOutput = new StringBuilder();
+            int size = this._boardSize;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    bool hasFigure = (_figures.Any(f => f.Row == i && f.Column == j));
+                    boardOutput.Append(hasFigure ? " *" : " _");
+                }
+
+                if (i < size - 1)
+                {
+                    boardOutput.AppendLine();
+                }
+            }
+            return boardOutput.ToString();
         }
 
     }
